ea0766f9e3f4edf9125682612683e0fc036e40c5
Arashad Ali <162456771+ArashadAli@users.noreply.github.com>
2026-07-03 22:19:33 +0530

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int len = s.length();
+        int[] store = new int[128];
+        Arrays.fill(store, -1);
+        int result = 0;
+        int left = 0;
+        for(int right = 0; right < len; right++) {
+            char ch = s.charAt(right);
+            if(store[ch] >= left) {
+                left = store[ch] + 1;
+            }
+            store[ch] = right;
+            result = Math.max(result, right - left + 1);
+        }
+        return result;
+    }
+}
