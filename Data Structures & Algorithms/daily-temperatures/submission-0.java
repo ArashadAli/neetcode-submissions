52a1164d77b98314a3265031f9404d8b28d69f81
Arashad Ali <162456771+ArashadAli@users.noreply.github.com>
2026-07-07 23:36:40 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        //This problem is based upon the next greater element problem
+
+        int len = temperatures.length;
+        int[] res = new int[len];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < len; i++) {
+
+            while (!stack.isEmpty() &&
+                   temperatures[i] > temperatures[stack.peek()]) {
+
+                int index = stack.pop();
+                res[index] = i - index;
+            }
+
+            stack.push(i);
+        }
+
+        return res;
+    }
+}
